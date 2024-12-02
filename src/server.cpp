094f098ccc3f599b91866546166d76f360ce2700
Sami <b.sami.code@gmail.com>
2024-12-02 18:52:17 +0100

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -1,199 +1,81 @@
 #include <iostream>
 #include <cstring>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <vector>
-#include <algorithm>
+#include "server_init.hpp"
+#include "dns_message.hpp"
+#include "request_handling.hpp"
 
-struct DNSHeader {
-    uint16_t id;
-    uint16_t flags;
-    uint16_t qdcount;
-    uint16_t ancount;
-    uint16_t nscount;
-    uint16_t arcount;
-};
-
-struct DNSQuestion {
-    std::vector<uint8_t> qname;
-    uint16_t qtype;
-    uint16_t qclass;
-};
-
-void create_dns_header(char* response, const DNSHeader& request_header, uint16_t qdcount, uint16_t ancount) {
-    DNSHeader response_header;
-    response_header.id = request_header.id;
-
-    uint16_t qr = 1;
-    uint16_t opcode = (ntohs(request_header.flags) >> 11) & 0xF;
-    uint16_t aa = 0;
-    uint16_t tc = 0;
-    uint16_t rd = (ntohs(request_header.flags) >> 8) & 0x1;
-    uint16_t ra = 0;
-    uint16_t z = 0;
-    uint16_t rcode = (opcode == 0) ? 0 : 4;
-
-    response_header.flags = htons((qr << 15) | (opcode << 11) | (aa << 10) | (tc << 9) | (rd << 8) | (ra << 7) | (z << 4) | rcode);
-
-    response_header.qdcount = htons(qdcount);
-    response_header.ancount = htons(ancount);
-    response_header.nscount = htons(0);
-    response_header.arcount = htons(0);
-
-    memcpy(response, &response_header, sizeof(DNSHeader));
-}
-
-DNSQuestion parse_dns_question(const char* buffer, size_t& offset) {
-    DNSQuestion question;
-    uint8_t length;
-
-    while (true) {
-        length = buffer[offset];
-        if ((length & 0xC0) == 0xC0) {  // Compressed label
-            offset += 2;  // Skip the pointer
-            break;
-        } else if (length == 0) {
-            break;
-        } else {
-            for (int i = 0; i < length; i++) {
-                question.qname.push_back(buffer[offset + i + 1]);
-            }
-            question.qname.push_back('.');
-            offset += length + 1;
-        }
-    }
-    if (!question.qname.empty()) {
-        question.qname.pop_back();  // Remove trailing dot
-    }
-    offset++;  // Skip the null byte
-
-    memcpy(&question.qtype, buffer + offset, 2);
-    question.qtype = ntohs(question.qtype);
-    offset += 2;
-
-    memcpy(&question.qclass, buffer + offset, 2);
-    question.qclass = ntohs(question.qclass);
-    offset += 2;
-
-    return question;
-}
-
-size_t create_question(char* response, const DNSQuestion& question) {
-    size_t offset = 0;
-    size_t i = 0;
-    while (i < question.qname.size()) {
-        auto dot_pos = std::find(question.qname.begin() + i, question.qname.end(), '.');
-        size_t length = dot_pos - (question.qname.begin() + i);
-        response[offset++] = length;
-        memcpy(response + offset, &question.qname[i], length);
-        offset += length;
-        i += length + 1;
-    }
-    response[offset++] = 0;  // Null terminator
-
-    uint16_t qtype = htons(question.qtype);
-    memcpy(response + offset, &qtype, 2);
-    offset += 2;
-
-    uint16_t qclass = htons(question.qclass);
-    memcpy(response + offset, &qclass, 2);
-    offset += 2;
-
-    return offset;
-}
-
-size_t create_answer(char* response, const DNSQuestion& question) {
-    size_t offset = create_question(response, question);
-
-    uint32_t ttl = htonl(60);  // TTL of 60 seconds
-    memcpy(response + offset, &ttl, 4);
-    offset += 4;
-
-    uint16_t rdlength = htons(4);  // Length of RDATA (4 bytes for IPv4)
-    memcpy(response + offset, &rdlength, 2);
-    offset += 2;
-
-    uint32_t ip = htonl(0x08080808);  // 8.8.8.8 as an example IP
-    memcpy(response + offset, &ip, 4);
-    offset += 4;
-
-    return offset;
-}
-
-int main() {
+int main(int argc, char** argv) {
+    // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    int udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (udpSocket == -1) {
-        std::cerr << "Socket creation failed: " << strerror(errno) << std::endl;
-        return 1;
+    // Disable output buffering
+    setbuf(stdout, NULL);
+
+    Resolver_Info resolver_info;
+    std::string resolver;
+    for (int i = 1; i < argc; i++) {
+        if(i + 1 != argc) {
+            if(strcmp(argv[i], "--resolver") == 0) {
+                resolver = argv[i + 1];
+                resolver_info.ip = resolver.substr(0, resolver.find(':'));
+                resolver_info.port = resolver.substr(resolver.find(':') + 1);
+                break;
+            }
+        }
     }
+    setup_resolver_socket(resolver_info);
 
-    int reuse = 1;
-    if (setsockopt(udpSocket, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)) < 0) {
-        std::cerr << "SO_REUSEPORT failed: " << strerror(errno) << std::endl;
-        return 1;
+    // First element of the pair is the code returned during server creation
+    // Second element of the pair is the resulting socket file descriptor
+    std::pair<int, int> server_status = create_server(2053);
+    if(server_status.first != 0) {
+        std::cerr<< "Server creation failed!\n";
+        return server_status.first;
     }
-
-    sockaddr_in serv_addr = {};
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(2053);
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    if (bind(udpSocket, reinterpret_cast<struct sockaddr*>(&serv_addr), sizeof(serv_addr)) != 0) {
-        std::cerr << "Bind failed: " << strerror(errno) << std::endl;
-        return 1;
-    }
-
-    char buffer[512];
-    char response[512];
+    int udpSocket = server_status.second;
     struct sockaddr_in clientAddress;
+
+    int bytesRead;
+    uint8_t buffer[512];
     socklen_t clientAddrLen = sizeof(clientAddress);
 
     while (true) {
-        int bytesRead = recvfrom(udpSocket, buffer, sizeof(buffer), 0,
-                                 reinterpret_cast<struct sockaddr*>(&clientAddress),
-                                 &clientAddrLen);
+        // Receive data
+        bytesRead = recvfrom(udpSocket, buffer, sizeof(buffer), 0, reinterpret_cast<struct sockaddr*>(&clientAddress), &clientAddrLen);
         if (bytesRead == -1) {
-            std::cerr << "Error receiving data: " << strerror(errno) << std::endl;
-            continue;
+            perror("Error receiving data");
+            break;
         }
 
-        if (bytesRead < sizeof(DNSHeader)) {
-            std::cerr << "Received packet is too small to be a valid DNS query" << std::endl;
-            continue;
+        buffer[bytesRead] = '\0';
+        std::cout << "Received " << bytesRead << " bytes: " << buffer << std::endl;
+
+        // Create the response
+        DNS_Message response = create_response(buffer);
+        int question_number = response.header.QDCOUNT;
+        response.to_network_order();
+        response.create_response_labels(question_number, buffer);
+
+        // Forward message to resolver if possible
+        int question_index{0};
+        while(!resolver_info.ip.empty() && question_index < question_number) {
+            query_resolver_server(resolver_info.socket, resolver_info.addr, response, question_index);
+            question_index++;
         }
 
-        DNSHeader* request_header = reinterpret_cast<DNSHeader*>(buffer);
-        size_t offset = sizeof(DNSHeader);
-        std::vector<DNSQuestion> questions;
+        std::pair<std::unique_ptr<uint8_t[]>, size_t> responseBuffer = create_response_buffer(question_number, response);
 
-        for (uint16_t i = 0; i < ntohs(request_header->qdcount); ++i) {
-            questions.push_back(parse_dns_question(buffer, offset));
-        }
-
-        // Create DNS response
-        create_dns_header(response, *request_header, questions.size(), questions.size());
-        size_t responseOffset = sizeof(DNSHeader);
-
-        for (const auto& question : questions) {
-            responseOffset += create_question(response + responseOffset, question);
-        }
-
-        for (const auto& question : questions) {
-            responseOffset += create_answer(response + responseOffset, question);
-        }
-
-        if (sendto(udpSocket, response, responseOffset, 0,
-                   reinterpret_cast<struct sockaddr*>(&clientAddress),
-                   sizeof(clientAddress)) == -1) {
-            std::cerr << "Failed to send response: " << strerror(errno) << std::endl;
+        // Send response
+        if (sendto(udpSocket, responseBuffer.first.get(), responseBuffer.second, 0, reinterpret_cast<struct sockaddr*>(&clientAddress), sizeof(clientAddress)) == -1) {
+            perror("Failed to send response");
         }
     }
 
     close(udpSocket);
+    close(resolver_info.socket);
+
     return 0;
 }
