b53b57d97c917210c68d441d5b763a885a9afebd
Sami <b.sami.code@gmail.com>
2024-12-02 18:57:14 +0100

    codecrafters submit [skip ci]

--- src/server.cpp
+++ src/server.cpp
@@ -6,72 +6,74 @@
 #include "dns_message.hpp"
 #include "request_handling.hpp"
 
-int main(int argc, char** argv) {
-    // Flush after every std::cout / std::cerr
+void configure_output() {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-
-    // Disable output buffering
     setbuf(stdout, NULL);
+}
 
+Resolver_Info parse_resolver_arg(int argc, char** argv) {
     Resolver_Info resolver_info;
-    std::string resolver;
-    for (int i = 1; i < argc; i++) {
-        if(i + 1 != argc) {
-            if(strcmp(argv[i], "--resolver") == 0) {
-                resolver = argv[i + 1];
-                resolver_info.ip = resolver.substr(0, resolver.find(':'));
-                resolver_info.port = resolver.substr(resolver.find(':') + 1);
-                break;
+    for (int i = 1; i < argc - 1; i++) {
+        if (strcmp(argv[i], "--resolver") == 0) {
+            std::string resolver = argv[i + 1];
+            size_t colon_pos = resolver.find(':');
+            if (colon_pos != std::string::npos) {
+                resolver_info.ip = resolver.substr(0, colon_pos);
+                resolver_info.port = resolver.substr(colon_pos + 1);
             }
-        }
-    }
-    setup_resolver_socket(resolver_info);
-
-    // First element of the pair is the code returned during server creation
-    // Second element of the pair is the resulting socket file descriptor
-    std::pair<int, int> server_status = create_server(2053);
-    if(server_status.first != 0) {
-        std::cerr<< "Server creation failed!\n";
-        return server_status.first;
-    }
-    int udpSocket = server_status.second;
-    struct sockaddr_in clientAddress;
-
-    int bytesRead;
-    uint8_t buffer[512];
-    socklen_t clientAddrLen = sizeof(clientAddress);
-
-    while (true) {
-        // Receive data
-        bytesRead = recvfrom(udpSocket, buffer, sizeof(buffer), 0, reinterpret_cast<struct sockaddr*>(&clientAddress), &clientAddrLen);
-        if (bytesRead == -1) {
-            perror("Error receiving data");
             break;
         }
+    }
+    return resolver_info;
+}
 
-        buffer[bytesRead] = '\0';
-        std::cout << "Received " << bytesRead << " bytes: " << buffer << std::endl;
+void handle_client_request(int udpSocket, Resolver_Info& resolver_info) {
+    struct sockaddr_in clientAddress;
+    socklen_t clientAddrLen = sizeof(clientAddress);
+    uint8_t buffer[512];
 
-        // Create the response
-        DNS_Message response = create_response(buffer);
-        int question_number = response.header.QDCOUNT;
-        response.to_network_order();
-        response.create_response_labels(question_number, buffer);
+    int bytesRead = recvfrom(udpSocket, buffer, sizeof(buffer), 0, 
+                             reinterpret_cast<struct sockaddr*>(&clientAddress), &clientAddrLen);
+    if (bytesRead == -1) {
+        perror("Error receiving data");
+        return;
+    }
 
-        // Forward message to resolver if possible
-        int question_index{0};
-        while(!resolver_info.ip.empty() && question_index < question_number) {
-            query_resolver_server(resolver_info.socket, resolver_info.addr, response, question_index);
-            question_index++;
-        }
+    buffer[bytesRead] = '\0';
+    std::cout << "Received " << bytesRead << " bytes: " << buffer << std::endl;
 
-        std::pair<std::unique_ptr<uint8_t[]>, size_t> responseBuffer = create_response_buffer(question_number, response);
+    DNS_Message response = create_response(buffer);
+    int question_number = response.header.QDCOUNT;
+    response.to_network_order();
+    response.create_response_labels(question_number, buffer);
 
-        // Send response
-        if (sendto(udpSocket, responseBuffer.first.get(), responseBuffer.second, 0, reinterpret_cast<struct sockaddr*>(&clientAddress), sizeof(clientAddress)) == -1) {
-            perror("Failed to send response");
-        }
+    for (int question_index = 0; !resolver_info.ip.empty() && question_index < question_number; ++question_index) {
+        query_resolver_server(resolver_info.socket, resolver_info.addr, response, question_index);
+    }
+
+    auto [responseBuffer, responseSize] = create_response_buffer(question_number, response);
+
+    if (sendto(udpSocket, responseBuffer.get(), responseSize, 0, 
+               reinterpret_cast<struct sockaddr*>(&clientAddress), sizeof(clientAddress)) == -1) {
+        perror("Failed to send response");
+    }
+}
+
+int main(int argc, char** argv) {
+    configure_output();
+
+    Resolver_Info resolver_info = parse_resolver_arg(argc, argv);
+    setup_resolver_socket(resolver_info);
+
+    auto [server_code, udpSocket] = create_server(2053);
+    if (server_code != 0) {
+        std::cerr << "Server creation failed!\n";
+        return server_code;
+    }
+
+    while (true) {
+        handle_client_request(udpSocket, resolver_info);
     }
 
     close(udpSocket);
